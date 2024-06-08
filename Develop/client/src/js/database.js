a8843054c6c561ed0757c33b24e2cebd6ef5b0b8
Doug Scheible <doug_scheible@yahoo.com>
2024-06-07 22:38:03 -0500

    still working on reading from db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database'); 
+  console.log(content);
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .put() method to update content in db
+  const data = { content };
+
+  // Ensure the data object does not have the 'id' property if using autoIncrement
+  /*if ('id' in data) {
+    delete data.id;
+  }*/
+
+  console.log(data);
+  const request = store.put(data);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 // Export a function we will use to GET to the database.
@@ -37,3 +64,6 @@ export const getDb = async () => {
   console.log('result.value', result);
   return result;
 };
+
+// Start the database
+const result = await initdb();
